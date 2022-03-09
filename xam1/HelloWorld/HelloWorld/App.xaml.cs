fa1c45dc810c4f507b4b28ffedc57fa76e32bbec
Artur Oczkowski <70267255+Artxt123@users.noreply.github.com>
2022-03-10 00:53:52 +0100

    3.2 Kontrolki

--- xam1/HelloWorld/HelloWorld/App.xaml.cs
+++ xam1/HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new FirstPage();
+            MainPage = new ControlsPage();
         }
 
         protected override void OnStart()
---
+++ xam1/HelloWorld/HelloWorld/Views/ControlsPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControlsPage : ContentPage
+    {
+        public ControlsPage()
+        {
+            InitializeComponent();
+
+            var label = new Label
+            {
+                Text = "My first label",
+                FontSize = 12,
+                FontAttributes = FontAttributes.Bold,
+                FontFamily = "Arial",
+                TextColor = Color.Blue
+            };
+            ContentContainer.Children.Add(label);
+
+            var entry = new Entry
+            {
+                Keyboard = Keyboard.Numeric,
+                Placeholder = "Numeric value"
+            };
+            ContentContainer.Children.Add(entry);
+
+            var button = new Button
+            {
+                Text = "Click me"
+            };
+            button.Clicked += Button_Clicked;
+            ContentContainer.Children.Add(button);
+
+            var checkbox = new CheckBox
+            {
+                IsChecked = false,
+                HorizontalOptions = LayoutOptions.End
+            };
+            ContentContainer.Children.Add(checkbox);
+
+            var switchControl = new Switch
+            {
+                IsToggled = true
+            };
+            ContentContainer.Children.Add(switchControl);
+
+            var datePicker = new DatePicker
+            {
+                MinimumDate = new DateTime(2022, 1, 1)
+            };
+            ContentContainer.Children.Add(datePicker);
+
+            List<string> daysList = new List<string>
+            {
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+            };
+
+            var picker = new Picker
+            {
+                ItemsSource = daysList,
+                Title = "Choose day"
+            };
+            ContentContainer.Children.Add(picker);
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
